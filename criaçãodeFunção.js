a3bae06a538ccdc981d59838ab0a3a4cc5dc3ac9
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-02-28 07:37:40 -0300

    Create criaçãodeFunção.js

---
+++ criaçãodeFunção.js
@@ -0,0 +1,10 @@
+const robo ={
+    braços: 4,
+    pernas: 2,
+    arma: 'metralhadotra',
+    armaEspecial: 'foguete',
+    atirar: function(){
+        console.log('pew pew pew')
+    }
+}
+robo.atirar();
