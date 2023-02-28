c4531376839ebc1573edf99842283223b089478c
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-02-28 07:37:44 -0300

    Create CriaçãodeObejeto.js

---
+++ CriaçãodeObejeto.js
@@ -0,0 +1,12 @@
+let carro ={
+    portas: 4,
+    cor: 'verde',   // propriedades do carro(atributos que o carro possue)
+    opicionais: [
+        "teto solar", "blindagem", "ar-condicionado",
+    ],
+    revisando:true // as propriedades aceitam qualquer tipo de dado no js, booleanos, numbers, strings, arrays
+}
+console.log(carro.portas);
+console.log(carro.cor);
+console.log(carro.opicionais);
+console.log(carro.opicionais);
