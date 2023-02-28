617bf6c9efbf102023de3590d6c47c7ca13d2ac2
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-02-28 07:37:48 -0300

    Create Propriedadescommaisdeumapalavra.js

---
+++ Propriedadescommaisdeumapalavra.js
@@ -0,0 +1,10 @@
+let carro ={
+    portas: 4,
+    cor: 'verde',
+    'tem blindagem':true
+
+}
+
+console.log(carro.portas);
+console.log(carro.cor);
+console.log(carro["tem blindafem"]);  // essa forma de utilização não e muito usada pelos programação
