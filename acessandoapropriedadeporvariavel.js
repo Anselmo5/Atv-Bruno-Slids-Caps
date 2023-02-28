1c22c14a933c9d05eba6c11e8d5f7bd6d8f142ad
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-02-28 07:37:35 -0300

    Create acessandoapropriedadeporvariavel.js

---
+++ acessandoapropriedadeporvariavel.js
@@ -0,0 +1,9 @@
+const robo ={
+    braços: 4,
+    pernas: 2,
+    arma: 'metralhadotra',
+    armaEspecial: 'foguete'
+}
+
+let a = 'arma' // podemos acessar uma propriedade por meio uma variável( essa Forma não e muito utilizada)
+console.log(robo[a]);
